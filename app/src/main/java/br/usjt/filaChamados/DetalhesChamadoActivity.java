745236e45aac3289fd105db7099cdf680648ad1d
arqdsis <arqdsis@MOOLI10307.animaedu.intranet>
2019-03-20 21:48:52 -0300

    aula 3.1

---
+++ app/src/main/java/br/usjt/filaChamados/DetalhesChamadoActivity.java
@@ -0,0 +1,13 @@
+package br.usjt.filaChamados;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalhesChamadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_chamado);
+    }
+}
